a3a87a12d1b25c1cdf5195cd5fb82c73308cae9e
Ryu0118 <87907656+Ryu0118@users.noreply.github.com>
2022-09-08 16:54:36 +0900

    add Formula

---
+++ Formula/magicib.rb
@@ -0,0 +1,29 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Magicib < Formula
+  desc "MagicIB is a CLI tool that quickly and automatically generates Swift code from Interface Builder files"
+  homepage ""
+  url "https://github.com/Ryu0118/MagicIB/releases/download/0.0.1/magicib"
+  sha256 "431441e78921e1f30a2d1e78150d6664b8adf3332325307d1de7c5f89f2171b1"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    bin.install "magicib"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test MagicIB`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
